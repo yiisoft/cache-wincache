e1a9419baab93684241bd3ef910453f904af3bbf
Alexander Kartavenko <askemailbox@gmail.com>
2019-08-23 15:44:29 +0300

    Fix the case when the key is the string representation of an integer (#5)

--- src/WinCache.php
+++ src/WinCache.php
@@ -20,12 +20,14 @@ class WinCache implements CacheInterface
 
     public function get($key, $default = null)
     {
+        $this->validateKey($key);
         $value = \wincache_ucache_get($key, $success);
         return $success ? $value : $default;
     }
 
     public function set($key, $value, $ttl = null): bool
     {
+        $this->validateKey($key);
         $ttl = $this->normalizeTtl($ttl);
         if ($ttl < 0) {
             return $this->delete($key);
@@ -35,6 +37,7 @@ class WinCache implements CacheInterface
 
     public function delete($key): bool
     {
+        $this->validateKey($key);
         return \wincache_ucache_delete($key);
     }
 
@@ -45,19 +48,33 @@ class WinCache implements CacheInterface
 
     public function getMultiple($keys, $default = null): iterable
     {
-        $defaultValues = array_fill_keys($this->iterableToArray($keys), $default);
-        return array_merge($defaultValues, \wincache_ucache_get($this->iterableToArray($keys)));
+        $keys = $this->iterableToArray($keys);
+        $this->validateKeys($keys);
+        $valuesFromCache = \wincache_ucache_get($keys);
+        $valuesFromCache = $this->normalizeWinCacheOutput($valuesFromCache);
+        $values = array_fill_keys($keys, $default);
+        foreach ($values as $key => $value) {
+            $values[$key] = $valuesFromCache[$key] ?? $value;
+        }
+
+        return $values;
     }
 
     public function setMultiple($values, $ttl = null): bool
     {
-        return \wincache_ucache_set($this->iterableToArray($values), null, $this->normalizeTtl($ttl)) === [];
+        $values = $this->iterableToArray($values);
+        $this->validateKeysOfValues($values);
+        $ttl = $this->normalizeTtl($ttl);
+
+        return \wincache_ucache_set($values, null, $ttl) === [];
     }
 
     public function deleteMultiple($keys): bool
     {
+        $keys = $this->iterableToArray($keys);
+        $this->validateKeys($keys);
         /** @var array $deleted */
-        $deleted = \wincache_ucache_delete($this->iterableToArray($keys));
+        $deleted = \wincache_ucache_delete($keys);
         $deleted = array_flip($deleted);
         foreach ($keys as $expectedKey) {
             if (!isset($deleted[$expectedKey])) {
@@ -69,6 +86,7 @@ class WinCache implements CacheInterface
 
     public function has($key): bool
     {
+        $this->validateKey($key);
         return \wincache_ucache_exists($key);
     }
 
@@ -94,12 +112,63 @@ class WinCache implements CacheInterface
     }
 
     /**
-     * Converts iterable to array
-     * @param iterable $iterable
+     * Converts iterable to array. If provided value is not iterable it throws an InvalidArgumentException
+     * @param mixed $iterable
      * @return array
      */
-    private function iterableToArray(iterable $iterable): array
+    private function iterableToArray($iterable): array
     {
+        if (!is_iterable($iterable)) {
+            throw new InvalidArgumentException('Iterable is expected, got ' . gettype($iterable));
+        }
+
         return $iterable instanceof \Traversable ? iterator_to_array($iterable) : (array)$iterable;
+    }
+
+    /**
+     * @param mixed $key
+     */
+    private function validateKey($key): void
+    {
+        if (!\is_string($key)) {
+            throw new InvalidArgumentException('Invalid key value.');
+        }
+    }
+
+    /**
+     * @param array $keys
+     */
+    private function validateKeys(array $keys): void
+    {
+        foreach ($keys as $key) {
+            $this->validateKey($key);
+        }
+    }
+
+    /**
+     * @param array $values
+     */
+    private function validateKeysOfValues(array $values): void
+    {
+        $keys = array_map('strval', array_keys($values));
+        $this->validateKeys($keys);
+    }
+
+    /**
+     * Normalizes keys returned from wincache_ucache_get in multiple mode. If one of the keys is an integer (123) or a string
+     * representation of an integer ('123') the returned key from the cache doesn't equal neither to an integer nor a
+     * string ($key !== 123 and $key !== '123'). Coping element from the returned array one by one to the new array
+     * fixes this issue.
+     * @param array $values
+     * @return array
+     */
+    private function normalizeWinCacheOutput(array $values): array
+    {
+        $normalizedValues = [];
+        foreach ($values as $key => $value) {
+            $normalizedValues[$key] = $value;
+        }
+
+        return $normalizedValues;
     }
 }
