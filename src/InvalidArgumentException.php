c98454ce72c7fdd000d57b78594393b7a8ae6a6e
skugarev <skugarev.andrey@gmail.com>
2020-06-04 13:01:05 +0300

    Add strict types (#8)

--- src/InvalidArgumentException.php
+++ src/InvalidArgumentException.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Yiisoft\Cache\WinCache;
 
 class InvalidArgumentException extends \RuntimeException implements \Psr\SimpleCache\InvalidArgumentException
